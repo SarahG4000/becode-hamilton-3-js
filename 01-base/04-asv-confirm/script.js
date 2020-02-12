4c6251cff462f3d516470e945034c1c5001300df
SarahG4000 <sarah.guillaume4000@gmail.com>
2020-02-12 11:33:13 +0100

    ajout des exercices 1.4 et 9.5

--- 01-base/04-asv-confirm/script.js
+++ 01-base/04-asv-confirm/script.js
@@ -10,5 +10,31 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+
+    Reponse();//pas a faire!
+    
+    function Reponse(){//pas de majuscule!
+
+        let age = prompt("Donne moi ton age");// les variables + prompt => ouvre une boite de dialogue
+        let sex = prompt("De quel sexe es tu ?");
+        let city = prompt("Tu vis dans quelle ville ?");
+                                                                                                    //ne pas mettre true ici car il sere a rien 
+        if(confirm("Tu es agés de " + age + ", tu es un(e) " + sex + " et tu viens de " + city) == true){// les conditons a remplire
+
+            return alert("Good job!");// si toute condition remplis return un message
+
+        }else{
+
+            return Reponse();//sinon return la function jusqu'a remplire les conditions.
+         }
+
+    }
+
+
+
+
+
+        
+
+
 })();
--- 09-misc/05-worst-ui-three/script.js
+++ 09-misc/05-worst-ui-three/script.js
@@ -11,4 +11,110 @@
 
 (() => {
     // your code here
+        // your code here
+        let vitesse = 100; // La vitesse a laquelle les chiffre defilerons
+        let valUn;
+        let valDeux;
+        let valTrois;
+        let valQuatre;
+        let btnUn = true;
+        let btnDeux = true;
+        let btnTrois = true;
+        let btnQuatre = true;
+    
+        function defilementDesNombres() {
+            valUn = document.getElementById('part-one').getAttribute('value');//recuperation de value qui vaut 00
+            if (btnUn) {
+                if (valUn == document.getElementById('part-one').getAttribute('data-max')) {//si on attiens data-max
+                    valUn = document.getElementById('part-one').getAttribute('data-min');//on retourne data-min
+                } else {
+                    valUn++;//sinon on valun plus un jusqu'a data-max
+                }
+            }
+    
+            if (btnDeux) {
+                valDeux = document.getElementById('part-two').getAttribute('value');
+                if (valDeux == document.getElementById('part-two').getAttribute('data-max')) {
+                    valDeux = document.getElementById('part-two').getAttribute('data-min');
+                } else {
+                    valDeux++;
+                }
+                if (valDeux < 10 && valDeux > 0) {
+                    valDeux = "0" + valDeux;
+                }
+            }
+    
+            if (btnTrois) {
+                valTrois = document.getElementById('part-three').getAttribute('value');
+                if (valTrois == document.getElementById('part-three').getAttribute('data-max')) {
+                    valTrois = document.getElementById('part-three').getAttribute('data-min');
+                } else {
+                    valTrois++;
+                }
+                if (valTrois < 10 && valTrois > 0) {
+                    valTrois = "0" + valTrois;
+                }
+            }
+    
+            if (btnQuatre) {
+                valQuatre = document.getElementById('part-four').getAttribute('value');
+                if (valQuatre == document.getElementById('part-four').getAttribute('data-max')) {
+                    valQuatre = document.getElementById('part-four').getAttribute('data-min');
+                } else {
+                    valQuatre++;
+                }
+                if (valQuatre < 10 && valQuatre > 0) {
+                    valQuatre = "0" + valQuatre;
+                }
+            }
+    
+            document.getElementById("target").innerHTML = "+" + valUn + valDeux + valTrois + valQuatre;
+            document.getElementById('part-one').setAttribute('value', valUn);
+            document.getElementById('part-two').setAttribute('value', valDeux);
+            document.getElementById('part-three').setAttribute('value', valTrois);
+            document.getElementById('part-four').setAttribute('value', valQuatre);
+            setTimeout(defilementDesNombres, vitesse);
+        }
+    
+        defilementDesNombres();
+    
+        document.getElementById("fix-part-one").addEventListener("click", () => {
+            if (btnUn == true) {
+                btnUn = false;/* si retourne false deffillement actif */
+                document.getElementById('fix-part-one').innerHTML = "Start";
+            } else {
+                btnUn = true;/* quand on appuis retourne true et arret du defillement */
+                document.getElementById('fix-part-one').innerHTML = "Stop";
+            }
+        });
+    
+        document.getElementById("fix-part-two").addEventListener("click", () => {
+            if (btnDeux == true) {
+                btnDeux = false;
+                document.getElementById('fix-part-two').innerHTML = "Start";
+            } else {
+                btnDeux = true;
+                document.getElementById('fix-part-two').innerHTML = "Stop";
+            }
+        });
+    
+        document.getElementById("fix-part-three").addEventListener("click", () => {
+            if (btnTrois == true) {
+                btnTrois = false;
+                document.getElementById('fix-part-three').innerHTML = "Start";
+            } else {
+                btnTrois = true;
+                document.getElementById('fix-part-three').innerHTML = "Stop";
+            }
+        });
+    
+        document.getElementById("fix-part-four").addEventListener("click", () => {
+            if (btnQuatre == true) {
+                btnQuatre = false;
+                document.getElementById('fix-part-four').innerHTML = "Start";
+            } else {
+                btnQuatre = true;
+                document.getElementById('fix-part-four').innerHTML = "Stop";
+            }
+        });
 })();
